44114e9590411313c080f02a198dad727695f419
Diego N. Cabrera <87397817+1Hagi@users.noreply.github.com>
2023-07-02 04:32:09 -0300

    Perfil - Efecto de mov. agregado

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
 console.log('hola');
 
-let perfil_boton = $('.btn-colapsar-perfil');
+const boton_perfil = document.querySelector('.btn-colapsar-perfil');
+console.log(boton_perfil);
 
+boton_perfil.addEventListener('click', function() {
+
+    document.getElementById('perfil-contenedor').classList.toggle('perfil-colapsar-active');
+    document.getElementById('perfil-contenedor').classList.toggle('perfil-colapsar');
+    document.getElementById('perfil-contenedor').classList.toggle('perfil-colapsar');
+    document.getElementsByClassName('perfil-img')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('perfil-descrp')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('perfil-titulo')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('perfil-nombre')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('perfil-link')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('perfil-link')[1].classList.toggle('colapsar-active');
+    document.getElementsByClassName('perfil-link')[2].classList.toggle('colapsar-active');
+    document.getElementsByClassName('fa-facebook')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('fa-github')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('fa-linkedin')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('main-contenedor')[0].classList.toggle('colapsar-active');
+
+})
