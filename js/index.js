7e27500c7fca5e729e6cca26563d10864d3a57cd
Diego N. Cabrera <87397817+1Hagi@users.noreply.github.com>
2023-07-06 21:10:22 -0300

    Comenzando sección mobile

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 const boton_perfil = document.querySelector('.btn-colapsar-perfil');
-console.log(boton_perfil);
 
 boton_perfil.addEventListener('click', function() {
 
@@ -17,3 +16,19 @@ boton_perfil.addEventListener('click', function() {
     document.getElementsByClassName('main-contenedor')[0].classList.toggle('colapsar-active');
 
 })
+
+if (screen.width >= 320 && screen.width <= 480 ) {
+
+    document.getElementById('perfil-contenedor').classList.toggle('perfil-colapsar-active');
+    document.getElementsByClassName('perfil-img')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('perfil-descrp')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('perfil-titulo')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('perfil-nombre')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('perfil-link')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('perfil-link')[1].classList.toggle('colapsar-active');
+    document.getElementsByClassName('perfil-link')[2].classList.toggle('colapsar-active');
+    document.getElementsByClassName('fa-facebook')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('fa-github')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('fa-linkedin')[0].classList.toggle('colapsar-active');
+    document.getElementsByClassName('main-contenedor')[0].classList.toggle('colapsar-active');
+}
